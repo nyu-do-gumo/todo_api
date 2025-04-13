5e6bc7e49491e142faa9dbf0ec8d5d191affd05c
keiichi kobayashi <k.kobayashi@logic-design.jp>
2025-04-13 11:43:30 +0900

    feat: ログイン機能を追加

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only('email', 'password');
+
+    if (!Auth::attempt($credentials)) {
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    $user = $request->user();
+    $tokenName = $request->input('token_name', 'default');
+
+    $token = $user->createToken($tokenName);
+
+    return response()->json(['token' => $token->plainTextToken]);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
