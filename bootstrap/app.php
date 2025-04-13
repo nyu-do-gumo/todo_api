9d285e01ba1443f7e2cb3a2c90b4526651d370ff
keiichi kobayashi <k.kobayashi@logic-design.jp>
2025-04-13 11:21:55 +0900

    feat: SanctumのSPA認証からToken認証に切り替え

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
-        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
